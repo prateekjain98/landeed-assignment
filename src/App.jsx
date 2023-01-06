1fa5162b2aac6aa9115096518d9abc0222d120c2
Prateek Jain <prateek@evolvfit.in>
2023-01-06 21:13:44 +0000

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countdown from './components/Countdown'
+import WorldClock from './components/WorldClock'
+
+function App() {
+
+  return (
+    <div className='mainContainer'>
+      <Countdown/>
+      <WorldClock/>
+    </div>
+  )
+}
+
+export default App
